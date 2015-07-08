0e51b123f61d7d52b038d8829377c3277414bb6f
s0m3chill <d.kordiyak@gmail.com>
2015-07-08 11:45:46 +0300

    Alpha version ready

--- FaceSwap/ViewController.swift
+++ FaceSwap/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var photoIV: UIImageView!
     var imagePicker = UIImagePickerController()
+    var imageFrames: [CGRect] = []
     
     // MARK: - User action
     
@@ -26,7 +28,88 @@ class ViewController: UIViewController {
     }
 
     @IBAction func swap(sender: UIButton) {
+        // detect all faces
+        var facesFromImage = detectFaces()
+        // swap faces randomly
+        for face in facesFromImage {
+            let randomFrameIndex = Int(arc4random_uniform(UInt32(imageFrames.count)))
+            let randomFrame = imageFrames[randomFrameIndex]
+            
+            // may be slow
+            drawAbovePhotoIv(face, frame: randomFrame)
+        }
+    }
+    
+    // MARK: - Detection
+    
+    func detectFaces() -> [UIImage] {
+        var faces: [UIImage] = []
        
+        // handle image orientation
+        var exifOrientation = 6
+        switch (photoIV.image!.imageOrientation) {
+        case .Up:
+            exifOrientation = 1
+            break;
+        case .Down:
+            exifOrientation = 3;
+            break;
+        case .Left:
+            exifOrientation = 8;
+            break;
+        case .Right:
+            exifOrientation = 6;
+            break;
+        case .UpMirrored:
+            exifOrientation = 2;
+            break;
+        case .DownMirrored:
+            exifOrientation = 4;
+            break;
+        case .LeftMirrored:
+            exifOrientation = 5;
+            break;
+        case .RightMirrored:
+            exifOrientation = 7;
+            break;
+        default:
+            break;
+        }
+       
+        var imageCI = CIImage(CGImage: photoIV.image!.CGImage)
+        var context = CIContext(options: [kCIContextUseSoftwareRenderer: true])     // BSXPCMessage prevention
+        var options = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+        var detector = CIDetector(ofType: CIDetectorTypeFace, context: context, options: options)
+        var features = detector.featuresInImage(imageCI, options: [CIDetectorImageOrientation: exifOrientation])
+        
+        println("faces count: \(features.count)")
+        
+        // for all faces
+        for f in features as! [CIFaceFeature] {
+            var faceRect = f.bounds
+            imageFrames.append(faceRect)
+            // crop them and save to image array
+            var croppedImageRef = CGImageCreateWithImageInRect(photoIV.image!.CGImage, faceRect)
+            var croppedImage: UIImage! = UIImage(CGImage: croppedImageRef)
+            
+            faces.append(croppedImage!)
+        }
+        
+        return faces
+    }
+    
+    // MARK: - Swapping faces
+    
+    func drawAbovePhotoIv(faceImage: UIImage, frame: CGRect) {
+        UIGraphicsBeginImageContext(photoIV.image!.size)
+        photoIV.image!.drawInRect(CGRectMake(0, 0, photoIV.image!.size.width, photoIV.image!.size.height))
+        faceImage.drawInRect(CGRectMake(photoIV.image!.size.width - faceImage.size.width, photoIV.image!.size.height - faceImage.size.height, faceImage.size.width, faceImage.size.height))
+        let res = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            self.photoIV.image = res
+        })
     }
     
     // MARK: - Extensions
