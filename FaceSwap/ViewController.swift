cd4e63da821feec847541104f622cd5d5d680b66
s0m3chill <d.kordiyak@gmail.com>
2016-11-13 23:12:49 +0200

    Stub photo case

--- FaceSwap/ViewController.swift
+++ FaceSwap/ViewController.swift
@@ -25,41 +25,43 @@ class ViewController: UIViewController {
         // get 2 faces and draw them above photoIV
         let facesFromImage = FaceManager.shared.facesFrom(imageView: photoIV)
         
-        let face0 = facesFromImage[0]   // Darko
-        let face1 = facesFromImage[1]   // Max
-        
+        let face0 = facesFromImage[0]
+        let face1 = facesFromImage[1]
         var frame0 = face0.frame
         var frame1 = face1.frame
         
-        print("0:  \(frame0),    1: \(frame1)")
-        
-        let image0 = face0.image
-        let image1 = face1.image
+        if frame0.size.width > frame1.size.width {
+            frame0.origin.x += frame0.size.width / 2
+            frame1.origin.y -= frame1.size.height / 2
+        } else {
+            frame0.origin.x += frame0.size.width / 2
+            frame1.origin.y -= frame1.size.height / 2
+        }
         
         let frame0Size = frame0.size
         frame0.size = frame1.size
         frame1.size = frame0Size
         
-        drawAbovePhotoIv(image0, frame: frame0)
-        drawAbovePhotoIv(image1, frame: frame1)
+        drawAbovePhotoIv(face0.image, frame: frame0)
+        drawAbovePhotoIv(face1.image, frame: frame1)
     }
     
-    private func faceFrame(_ face: Face) -> CGRect {
-        var faceViewBounds = face.frame
-        
-        let ciImageSize = CIImage(image: photoIV.image!)!.extent.size
-        let viewSize = photoIV.bounds.size
-        let scale = min(viewSize.width / ciImageSize.width,
-                        viewSize.height / ciImageSize.height)
-        let offsetX = (viewSize.width - ciImageSize.width * scale) / 2
-        let offsetY = (viewSize.height - ciImageSize.height * scale) / 2
-        
-        //faceViewBounds = faceViewBounds.applying(CGAffineTransform(scaleX: scale, y: scale))
-        faceViewBounds.origin.x += offsetX
-        faceViewBounds.origin.y += offsetY
-        
-        return faceViewBounds
-    }
+//    private func faceFrame(_ face: Face) -> CGRect {
+//        var faceViewBounds = face.frame
+//        
+//        let ciImageSize = CIImage(image: photoIV.image!)!.extent.size
+//        let viewSize = photoIV.bounds.size
+//        let scale = min(viewSize.width / ciImageSize.width,
+//                        viewSize.height / ciImageSize.height)
+//        let offsetX = (viewSize.width - ciImageSize.width * scale) / 2
+//        let offsetY = (viewSize.height - ciImageSize.height * scale) / 2
+//        
+//        //faceViewBounds = faceViewBounds.applying(CGAffineTransform(scaleX: scale, y: scale))
+//        faceViewBounds.origin.x += offsetX
+//        faceViewBounds.origin.y += offsetY
+//        
+//        return faceViewBounds
+//    }
     
     private func drawAbovePhotoIv(_ faceImage: UIImage, frame: CGRect) {
         UIGraphicsBeginImageContext(photoIV.image!.size)
