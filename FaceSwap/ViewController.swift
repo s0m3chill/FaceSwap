c3f5b7732cfbc1536e5712d006545fc500c00bd7
s0m3chill <d.kordiyak@gmail.com>
2016-11-13 22:36:08 +0200

    Image sizes

--- FaceSwap/ViewController.swift
+++ FaceSwap/ViewController.swift
@@ -25,25 +25,49 @@ class ViewController: UIViewController {
         // get 2 faces and draw them above photoIV
         let facesFromImage = FaceManager.shared.facesFrom(imageView: photoIV)
         
-        let face0 = facesFromImage[0]
-        let face1 = facesFromImage[1]
+        let face0 = facesFromImage[0]   // Darko
+        let face1 = facesFromImage[1]   // Max
         
         var frame0 = face0.frame
         var frame1 = face1.frame
         
-        let frame0Size = frame0.size
-        frame1.size = frame0.size
-        frame0.size = frame0Size
+        print("0:  \(frame0),    1: \(frame1)")
         
-        drawAbovePhotoIv(face0.image, frame: frame0)
-        drawAbovePhotoIv(face1.image, frame: frame1)
+        let image0 = face0.image
+        let image1 = face1.image
+        
+        let frame0Size = frame0.size
+        frame0.size = frame1.size
+        frame1.size = frame0Size
+        
+        drawAbovePhotoIv(image0, frame: frame0)
+        drawAbovePhotoIv(image1, frame: frame1)
+    }
+    
+    private func faceFrame(_ face: Face) -> CGRect {
+        var faceViewBounds = face.frame
+        
+        let ciImageSize = CIImage(image: photoIV.image!)!.extent.size
+        let viewSize = photoIV.bounds.size
+        let scale = min(viewSize.width / ciImageSize.width,
+                        viewSize.height / ciImageSize.height)
+        let offsetX = (viewSize.width - ciImageSize.width * scale) / 2
+        let offsetY = (viewSize.height - ciImageSize.height * scale) / 2
+        
+        //faceViewBounds = faceViewBounds.applying(CGAffineTransform(scaleX: scale, y: scale))
+        faceViewBounds.origin.x += offsetX
+        faceViewBounds.origin.y += offsetY
+        
+        return faceViewBounds
     }
     
     private func drawAbovePhotoIv(_ faceImage: UIImage, frame: CGRect) {
         UIGraphicsBeginImageContext(photoIV.image!.size)
+        
         photoIV.image!.draw(in: CGRect(x: 0, y: 0, width: photoIV.image!.size.width, height: photoIV.image!.size.height))
-        faceImage.draw(in: CGRect(x: frame.origin.x, y: frame.origin.y, width: faceImage.size.width, height: faceImage.size.height))
+        faceImage.draw(in: CGRect(x: frame.origin.x, y: frame.origin.y, width: frame.size.width, height: frame.size.height))
         let res = UIGraphicsGetImageFromCurrentImageContext()
+        
         UIGraphicsEndImageContext()
         
         photoIV.image = res
